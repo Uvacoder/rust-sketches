ddac06f4f70d2ca772292a367020eb3b6bc18380
Alexander Gonzalez <alexfertel97@gmail.com>
2021-06-23 14:24:31 -0400

    Improve noise scaling

--- src/main.rs
+++ src/main.rs
@@ -18,8 +18,9 @@ fn model(app: &App) -> Model {
 
     let _window = app
         .new_window()
-        .size(1920, 1080)
+        .size(1024, 768)
         .view(view)
+        .key_pressed(key_pressed)
         .build()
         .unwrap();
 
@@ -35,10 +36,10 @@ fn view(app: &App, model: &Model, frame: Frame) {
     let mut rng = StdRng::seed_from_u64(model.seed);
     let noise = nannou::noise::Perlin::new().set_seed(model.seed as u32);
 
-    let line_count = 10000;
-    let min_vertices_per_line = 10;
+    let line_count = 3000;
+    let min_vertices_per_line = 5;
     let max_vertices_per_line = 30;
-    let step_size = 2.5;
+    let step_size = 5.0;
 
     let lines = (0..line_count).map(|_| {
         let mut point = pt2(
@@ -47,11 +48,14 @@ fn view(app: &App, model: &Model, frame: Frame) {
         );
         let mut line: Vec<Vec2> = vec![point];
 
-        for _ in 0..(max_vertices_per_line - min_vertices_per_line) {
-            let scaled_x = point[0] * 0.005;
-            let scaled_y = point[1] * 0.005;
+        for _ in 0..(rng.gen_range(min_vertices_per_line..max_vertices_per_line)) {
+            let scaled_x = point[0] * 0.0005;
+            let scaled_y = point[1] * 0.0005;
             let noise_value = noise.get([scaled_x as f64, scaled_y as f64]);
+            println!("Noise value: {}", noise_value);
+
             let angle = map_range(noise_value, 0.0, 1.0, 0.0, PI * 2.0 as f32);
+            println!("Angle: {}", angle);
 
             point = pt2(
                 point[0] + step_size * angle.sin(),
@@ -83,4 +87,10 @@ fn captured_frame_path(app: &App, frame: &Frame) -> std::path::PathBuf {
         .join(app.exe_name().unwrap())
         .join(format!("{:03}", frame.nth()))
         .with_extension("png")
+}
+
+fn key_pressed(app: &App, _model: &mut Model, key: Key) {
+    if key == Key::Q {
+        app.quit();
+    }
 }
